6c6e30dcccbc509736ae0ea1b6eba58390b7dd70
Is0meone <115733083+Is0meone@users.noreply.github.com>
2025-04-16 13:59:13 +0200

    Update frontend_nginx.sh

--- frontend_nginx.sh
+++ frontend_nginx.sh
@@ -44,7 +44,8 @@ echo "Lokalny Nginx skonfigurowany na porcie $API_PORT."
 echo "Instalacja Node.js (nvm)..."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 16
 
 echo "Klonowanie repozytorium front-end..."
